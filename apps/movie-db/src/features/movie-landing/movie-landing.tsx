18b33d814f492de126df366d6eb3ea45ce120aca
Agustin Garcia <agusgb93@gmail.com>
2022-07-26 09:54:34 +0200

    Add tailwind

--- apps/movie-db/src/features/movie-landing/movie-landing.tsx
+++ apps/movie-db/src/features/movie-landing/movie-landing.tsx
@@ -1,12 +1,15 @@
+import MovieItem from '../../ui/movie-item/movie-item';
 import styles from './movie-landing.module.scss';
 
 /* eslint-disable-next-line */
 export interface MovieLandingProps {}
 
 export function MovieLanding(props: MovieLandingProps) {
+  const myMovies = [...Array(9).keys()];
+
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to MovieLanding!</h1>
+    <div className="p-4">
+      {myMovies.map(movie => (<MovieItem/>))}
     </div>
   );
 }
