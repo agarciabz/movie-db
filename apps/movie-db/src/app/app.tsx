ff3d03f37821cf58cca54e45b3e5abedd1cba17b
Agustin Garcia <agusgb93@gmail.com>
2022-07-26 09:35:24 +0200

    Add navigation

--- apps/movie-db/src/app/app.tsx
+++ apps/movie-db/src/app/app.tsx
@@ -1,13 +1,19 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Navigate, Route, Routes } from 'react-router-dom';
+import MovieDetail from '../features/movie-detail/movie-detail';
+import MovieLanding from '../features/movie-landing/movie-landing';
 import styles from './app.module.scss';
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
-    <>
-      <NxWelcome title="movie-db" />
-      <div />
-    </>
+    <Routes>
+      <Route path="/search" element={<MovieLanding/>}></Route>
+      <Route path="/movie">
+        <Route path=":movieId" element={<MovieDetail/>}></Route>
+      </Route>
+      <Route path="*" element={<Navigate to="search" replace/>}></Route>
+    </Routes>
   );
 }
 
