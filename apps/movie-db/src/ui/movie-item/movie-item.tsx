7a898ea521301ec4b527b905ed6cb364d053e6cd
agarciabz <agusgb93@gmail.com>
2022-07-28 09:47:45 +0200

    Update card graphics

--- apps/movie-db/src/ui/movie-item/movie-item.tsx
+++ apps/movie-db/src/ui/movie-item/movie-item.tsx
@@ -5,16 +5,19 @@ export interface MovieItemProps {
 }
 
 export function MovieItem(props: MovieItemProps) {
-  const { poster, title } = props.movie;
+  const { poster, title, year } = props.movie;
 
   return (
     <div
-      className="p-2 flex flex-col space-y-2 border rounded shadow
+      className="flex flex-col border rounded shadow
       hover:shadow-xl hover:cursor-pointer
     hover:bg-sky-200 hover:shadow-sky-200 hover:border-sky-200"
     >
-      <img alt={title} src={poster}></img>
-      <div>{title}</div>
+      <img className="w-full h-64 object-cover" alt={title} src={poster}></img>
+      <div className="p-2">
+        <div className="font-semibold">{title}</div>
+        <div className="text-slate-500 text-sm">{year}</div>
+      </div>
     </div>
   );
 }
